842d8a8180a766dcefc01027eb30b664fd71b339
Erfan <erfan@erfan.com>
2020-12-27 20:09:10 +0330

    Translate to Persian

--- handlers/message.py
+++ handlers/message.py
@@ -13,16 +13,16 @@ async def message(client, message):
     if not is_youtube(message.text):
         await message.reply_text("❌لینک داده شده مشکل داره دوباره بفرست")
         return
-
+    
     if "list=" in message.text:
         await message.reply_text("یه لینک موزیک ویدیو بفرست نه یک پلی لیست!🤨")
         return
-
+    
     await message.reply_text("موزیک در صف دانلوده صبر کن👀", quote=True)
     download(
         (
             message.reply_text,
-            ("موزیکی که دادی داره دانلود میشه😉",
+            ("موزیکی که دادی داره دانلود میشه😉",)
         ),
         (
             message.reply_text,
@@ -34,34 +34,32 @@ async def message(client, message):
                 None,
                 (
                     message.reply_text,
-                                        ("این موزیک الان داره توی ویس چت پخش میشه🤩🎧",)
+                    ("این موزیک الان داره توی ویس چت پخش میشه🤩🎧",)
                 ),
                 (
-                   message.reply_text,
+                    message.reply_text,
                     ("موزیک به اتمام رسید😊❤️",)
                 ),
                 None,
                 None,
                 message.from_user.id,
                 message.from_user.first_name,
-                (
-                   client.send_message,
+                [
+                    client.send_message,
                     [
                         LOG_GROUP,
                         "<b>هم اکنون داره در ویس چت پخش میشه🤩🎧</b>\n"
                         "موزیک🎼: <a href=\"{}\">{}</a>\n"
                         "درخواست شده توسط😁: <a href=\"tg://user?id={}\">{}</a>"
                     ]
-					if LOG_GROUP else None
+                ] if LOG_GROUP else None
             ]
         ],
-        (
-            message.reply_text,
-            "نمیتونی که ویدیو های استریم (پخش زنده) رو پخش کنی!"
-        ),
         message.text,
     )
-	__handlers__ = [
+
+
+__handlers__ = [
     [
         MessageHandler(
             message,
